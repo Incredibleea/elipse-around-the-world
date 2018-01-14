b4c35b4c4f6e3142b9fbb6e15cf891f5ad11e01c
Dawid Gadomski <dw.gadomski@gmail.com>
2018-01-14 12:48:26 +0100

    Pliki, na ktorych znacznie lepiej widac wypisywanie - nie za duze?

--- ElipseRectangleCoincidence/src/elipserectangle/Recognizer.java
+++ ElipseRectangleCoincidence/src/elipserectangle/Recognizer.java
@@ -4,7 +4,7 @@ import java.util.ArrayList;
 import java.util.List;
 
 /*
- * Recognizer class is responisble for recognize shapes
+ * Recognizer class is responsible for recognize shapes
  */
 
 class Point {
