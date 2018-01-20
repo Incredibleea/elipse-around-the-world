524f24d192abe79ebd1c63fe09ed15c1a672885a
Dawid Gadomski <dw.gadomski@gmail.com>
2018-01-20 13:24:07 +0100

    Komentarze do dokumentacji dla mniej istotnych klas.

--- ElipseRectangleCoincidence/src/elipserectangle/Start.java
+++ ElipseRectangleCoincidence/src/elipserectangle/Start.java
@@ -1,7 +1,20 @@
 package elipserectangle;
 
+/**
+ * Klasa Start jest glowna klasa w projekcie. W niej tworzone sa obiekty pozostalych klas
+ * i wywolywane sa metody odpowiedzialne za odczytywanie pliku, rozpoznawanie elips, prostokatow
+ * oraz umozliwiona jest zmiana ich polozenia.
+ * 
+ * @author Wojciech Nowak, Dawid Gadomski
+ *
+ */
 public class Start {
 	
+	/**
+	 * Metoda glowna klasy Start.
+	 * 
+	 * @param args
+	 */
 	public static void main(String[] args) {
 		PixelArray p = PixelArray.getInstance();
 		p.printPixelArray();
